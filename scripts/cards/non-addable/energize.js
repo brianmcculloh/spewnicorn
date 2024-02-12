0f60e73671e3009fc2db2eb31ce18645604163aa
Christopher McCulloh <cmcculloh@gmail.com>
2024-02-12 09:34:53 -0500

    next batch

---
+++ scripts/cards/non-addable/energize.js
@@ -0,0 +1,10 @@
+export default {
+    id: 'energize', name: 'Energize', type: 'tool', mana: 0, tier: 'uncommon', vanish: true, addable: false,
+    actions: [
+        {action: 'stat', what: 'mana', key: 'current', value: 1}
+    ],
+    slots: 1,
+    shardUpgrades: {
+        retain: true
+    },
+}
